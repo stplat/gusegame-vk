eea564d1f502feed2977871a9b4b05f6bac8624a
stsp <st.platonov@gmail.com>
2024-01-26 15:51:53 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,59 +1,92 @@
-import React, { useState, useEffect } from 'react';
-import bridge from '@vkontakte/vk-bridge';
-import { View, ScreenSpinner, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol } from '@vkontakte/vkui';
-import '@vkontakte/vkui/dist/vkui.css';
+import React, { useState, useEffect } from "react";
+import bridge from "@vkontakte/vk-bridge";
+import {
+  View,
+  ScreenSpinner,
+  AdaptivityProvider,
+  AppRoot,
+  ConfigProvider,
+  SplitLayout,
+  SplitCol,
+  Group,
+  Cell,
+  SimpleCell,
+  Separator,
+  Spacing,
+} from "@vkontakte/vkui";
+import "@vkontakte/vkui/dist/vkui.css";
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
+import Home from "./panels/Home";
+import Persik from "./panels/Persik";
+import {
+  Icon20Add,
+  Icon20ListPenOutline,
+  Icon28Notifications,
+} from "@vkontakte/icons";
 
 const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+  const [activePanel, setActivePanel] = useState("home");
+  const [fetchedUser, setUser] = useState(null);
+  const [popout, setPopout] = useState(<ScreenSpinner size="large" />);
 
-	useEffect(() => {
-		bridge.send('VKWebAppSendPayload', {
-			group_id: 214250585, 
-			payload: {
-			}})
-			.then((data) => { 
-			  if (data.result) {
-				// Событие отправлено
-			  }
-			})
-			.catch((error) => {
-			  // Ошибка
-			  console.log(error);
-			});
-			
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
+  useEffect(() => {
+    bridge
+      .send("VKWebAppSendPayload", {
+        group_id: 214250585,
+        payload: {},
+      })
+      .then((data) => {
+        if (data.result) {
+          // Событие отправлено
+        }
+      })
+      .catch((error) => {
+        // Ошибка
+        console.log(error);
+      });
 
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
+    async function fetchData() {
+      const user = await bridge.send("VKWebAppGetUserInfo");
+      setUser(user);
+      setPopout(null);
+    }
+    fetchData();
+  }, []);
 
-	return (
-		<ConfigProvider>
-			<AdaptivityProvider>
-				<AppRoot>
-					<SplitLayout popout={popout}>
-						<SplitCol>
-							<View activePanel={activePanel}>
-								<Home id='home' fetchedUser={fetchedUser} go={go} />
-								<Persik id='persik' go={go} />
-							</View>
-						</SplitCol>
-					</SplitLayout>
-				</AppRoot>
-			</AdaptivityProvider>
-		</ConfigProvider>
-	);
-}
+  const go = (e) => {
+    setActivePanel(e.currentTarget.dataset.to);
+  };
+
+  return (
+    <ConfigProvider>
+      <AdaptivityProvider>
+        <AppRoot>
+          <Spacing />
+          <SplitLayout popout={popout} style={{ justifyContent: "center" }}>
+            <SplitCol
+              fixed
+              width={240}
+              maxWidth={240}
+              style={{ marginRight: 5 }}
+            >
+              <Group>
+                <SimpleCell before={<Icon20ListPenOutline />}>
+                  История закупок
+                </SimpleCell>
+                <SimpleCell before={<Icon20Add />}>Создать закупку</SimpleCell>
+              </Group>
+            </SplitCol>
+            <SplitCol width="100%" maxWidth="calc(100% - 260px)">
+              <View activePanel={activePanel}>
+                <Home id="home" fetchedUser={fetchedUser} go={go} />
+                <Persik id="persik" go={go} />
+              </View>
+            </SplitCol>
+          </SplitLayout>
+        </AppRoot>
+      </AdaptivityProvider>
+    </ConfigProvider>
+  );
+};
 
 export default App;
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { Panel, PanelHeader, Title, Button, Group, Cell, Div, Avatar, Card, CardGrid, Banner, Image } from '@vkontakte/vkui';
+import {
+  Panel,
+  PanelHeader,
+  Title,
+  Button,
+  Group,
+  Cell,
+  Div,
+  Avatar,
+  Card,
+  CardGrid,
+  Banner,
+  Image,
+} from "@vkontakte/vkui";
 
-import diablo from '../img/diablo4.jpg';
+import diablo from "../img/diablo4.jpg";
 
 const Home = ({ id, go, fetchedUser }) => (
-	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
-		<Group >
-			<Div>
-				<Title level="2" >
-					Каталог игр:
-				</Title>
-			</Div>
-			<Banner
-				before={
-					<Image
-					size={96}
-					src={diablo} 
-					/>
-				}
-				header="Diablo 4"
-				subheader="Турция"
-				text="5 990р"
-				actions={<Button>Подробнее</Button>}
-			/>
+  <Panel id={id} style={{ height: 1000 }}>
+    <PanelHeader>Example</PanelHeader>
+    <Group>
+      <Div>
+        <Title level="2">Каталог игр:</Title>
+      </Div>
+      <Banner
+        before={<Image size={96} src={diablo} />}
+        header="Diablo 4"
+        subheader="Турция"
+        text="5 990р"
+        actions={<Button>Подробнее</Button>}
+      />
 
-			{/* <CardGrid size="s">
+      {/* <CardGrid size="s">
 				<Card>
 					<div><img className="ProductCard" src={diablo} alt="Persik The Cat"/></div>
 				</Card>
@@ -38,8 +44,8 @@ const Home = ({ id, go, fetchedUser }) => (
 					<div style={{ paddingBottom: '92%' }} />
 				</Card>
 			</CardGrid> */}
-		</Group>
-		{/* {fetchedUser &&
+    </Group>
+    {/* {fetchedUser &&
 		<Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
 			<Cell
 				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
@@ -56,20 +62,20 @@ const Home = ({ id, go, fetchedUser }) => (
 				</Button>
 			</Div>
 		</Group> */}
-	</Panel>
+  </Panel>
 );
 
 Home.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
-	fetchedUser: PropTypes.shape({
-		photo_200: PropTypes.string,
-		first_name: PropTypes.string,
-		last_name: PropTypes.string,
-		city: PropTypes.shape({
-			title: PropTypes.string,
-		}),
-	}),
+  id: PropTypes.string.isRequired,
+  go: PropTypes.func.isRequired,
+  fetchedUser: PropTypes.shape({
+    photo_200: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    city: PropTypes.shape({
+      title: PropTypes.string,
+    }),
+  }),
 };
 
 export default Home;
