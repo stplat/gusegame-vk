07111f5f976c0b8356466486349e0bc5bb666a73
stsp <st.platonov@gmail.com>
2024-03-01 11:38:30 +0300

    Поправил путь

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Tearms from "./panels/Tearms/Tearms";
 import Contacts from "./panels/Contacts/Contacts";
 import Search from "./panels/Search/Search";
 
-const api = "http://127.0.0.1:8000/api/vk";
+const api = "https://api.gusegame.ru/api/vk";
 
 const App = () => {
   const [activePanel, setActivePanel] = useState("home");
